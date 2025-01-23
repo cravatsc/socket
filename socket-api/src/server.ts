868a152267b2e0774d381e5930509ddee177b003
Craig Cravath <craig.cravath@willowtreeapps.com>
2025-01-23 13:33:22 -0800

    clean up docker compose and api ports

--- socket-api/src/server.ts
+++ socket-api/src/server.ts
@@ -8,6 +8,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+server.listen(3001, () => {
+  console.log("Server is running on port 3001");
 });
